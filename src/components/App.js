37e6c4278203ca042c6bb4a62bd2c08f2d531b14
cheongjin.kim <devjin0617@gmail.com>
2016-08-04 00:08:12 +0900

    Add materialize css in application page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import 'materialize-loader';
+import 'materialize-css/bin/materialize';
+
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello React Application</h1>
+            <div>
+                <button className="waves-effect waves-light btn">hello</button>
+                <h1>Hello React Application</h1>
+            </div>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import 'materialize-loader'
+
+
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
