0c138c89a7b6e8b379528b330c547401b4c411e1
devjin0617 <devjin0617@gmail.com>
2016-08-21 23:23:39 +0900

    Add firebase

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 //import 'jquery';
 import 'materialize-loader';
 
+import firebase from 'firebase';
+
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
