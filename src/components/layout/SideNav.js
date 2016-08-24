c40682570a9094564bd285db2ac8b7a433a71b11
devjin0617 <devjin0617@gmail.com>
2016-08-24 23:34:41 +0900

    Modify menu list at left side menu

--- src/components/layout/SideNav.js
+++ src/components/layout/SideNav.js
@@ -9,14 +9,14 @@ class SideNav extends React.Component {
                     <li><div className="userView">
                         <img className="background" src="http://materializecss.com/images/office.jpg" />
                             <a href="#!user"><img className="circle" src="http://materializecss.com/images/yuna.jpg" /></a>
-                            <a href="#!name"><span className="white-text name">John Doe</span></a>
-                            <a href="#!email"><span className="white-text email">jdandturk@gmail.com</span></a>
+                            <a href="#!name"><span className="white-text name">Jin</span></a>
+                            <a href="#!email"><span className="white-text email">me@puding.kr</span></a>
                     </div></li>
-                    <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
-                    <li><a href="#!">Second Link</a></li>
+                    <li><a href="#!"><i className="material-icons">cloud</i>처음으로</a></li>
+                    <li><a href="#!">휴가신청</a></li>
                     <li><div className="divider"></div></li>
-                    <li><a className="subheader">Subheader</a></li>
-                    <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
+                    <li><a className="subheader">신청목록보기</a></li>
+                    <li><a className="waves-effect" href="#!">달력보기</a></li>
                 </ul>
             </div>
         );
