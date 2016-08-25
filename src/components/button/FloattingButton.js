cf8a89accf35b898e0d20d9b9c42c86b2a1e626e
devjin0617 <devjin0617@gmail.com>
2016-08-25 23:30:28 +0900

    Add tooltip to left in floatting button

--- src/components/button/FloattingButton.js
+++ src/components/button/FloattingButton.js
@@ -10,10 +10,8 @@ class FloattingButton extends React.Component {
                     <i className="large material-icons">mode_edit</i>
                 </a>
                 <ul>
-                    <li><a className="btn-floating red"><i className="material-icons">insert_chart</i></a></li>
-                    <li><a className="btn-floating yellow darken-1"><i className="material-icons">format_quote</i></a></li>
-                    <li><a className="btn-floating green"><i className="material-icons">publish</i></a></li>
-                    <li><a className="btn-floating blue"><i className="material-icons">attach_file</i></a></li>
+                    <li><a className="btn-floating red tooltipped" data-position="left" data-delay="50" data-tooltip="빠른휴가신청"><i className="material-icons">insert_chart</i></a></li>
+                    <li><a className="btn-floating yellow darken-1 tooltipped" data-position="left" data-delay="50" data-tooltip="짧은 글 남기기"><i className="material-icons">format_quote</i></a></li>
                 </ul>
             </div>
         );
