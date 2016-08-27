e714f97037f83cf52df10f8de0377debebe1cac3
developerjin <devjin0617@gmail.com>
2016-08-27 17:07:07 +0900

    Remove jquery and firebase lib

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-//import 'jquery';
 import 'materialize-loader';
-
-//import firebase from 'firebase';
 
 
 const rootElement = document.getElementById('root');
